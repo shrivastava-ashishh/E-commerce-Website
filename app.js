0d67d748266fdb3197ade49f724afeba0053b437
Ashish Shrivastava <smartashishg442@gmail.com>
2025-01-12 20:14:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+
+const sign_up_btn = document.querySelector("#sign-up-btn");
+
+const container = document.querySelector(".container");
+
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
+sign_up_btn2.addEventListener("click", () => {
+    container.classList.add("sign-up-mode2");
+});
+
+sign_in_btn2.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode2");
+});
